958bc7b6143b8070e85f0e370063b43dd166dc38
Belal Sameh <belalsameh.dev@gmail.com>
2026-01-13 01:22:49 +0200

    feat(home): build brand slider component

--- src/components/BrandSlider/BrandSlider.jsx
+++ src/components/BrandSlider/BrandSlider.jsx
@@ -1,10 +1,70 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./BrandSlider.css";
 
 function BrandSlider() {
+  const settings = {
+    dots: false,
+    arrow: false,
+    infinite: true,
+    swipeToSlide: true,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1285,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 390,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+        },
+      },
+    ],
+  };
+
+  function getAllBrands() {
+    return axios.get("https://ecommerce.routemisr.com/api/v1/brands");
+  }
+  const { data: allBrands } = useQuery({
+    queryKey: ["allBrands"],
+    queryFn: getAllBrands,
+    select: (res) => res.data.data
+  });
+
   return (
-    <section className="brand-slider container">
-      <h1 className="section-title">BrandSlider</h1>
-    </section>
+    <div className="brand-slider">
+      <h2 className="sub-title">featured brands</h2>
+      <Slider {...settings}>
+        {allBrands?.map((brand) => (
+          <div key={brand._id} className="grab">
+            <img src={brand.image} alt={brand.name} />
+            <h6>{brand.name}</h6>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 }
 

0f3bfb778925d9d032e3af13e172c6a3f9a5828b
Belal Sameh <belalsameh.dev@gmail.com>
2026-01-13 01:11:13 +0200

    feat(orders): build orders page with reusable order item

--- src/components/OrderItem/OrderItem.jsx
+++ src/components/OrderItem/OrderItem.jsx
@@ -1,10 +1,67 @@
-import "./orderItem.css";
+import "./OrderItem.css";
 
-function OrderItem() {
+function OrderItem({ order }) {
+  const {
+    id: orderId,
+    createdAt: date,
+    totalOrderPrice: total,
+    paymentMethodType: method,
+    cartItems,
+  } = order;
+
+  const formattedDate = new Date(date).toLocaleString("en-US", {
+    timeZone: "Egypt",
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  });
+
   return (
-    <section className="order-item container">
-      <h1 className="section-title">OrderItem</h1>
-    </section>
+    <div className="order-item bg-body-tertiary p-4">
+      <div className="order-info">
+        <div>
+          <span>Order ID</span>
+          <strong>{`#${orderId}`}</strong>
+        </div>
+        <div>
+          <span>Date</span>
+          <strong>{formattedDate}</strong>
+        </div>
+        <div>
+          <span>Total</span>
+          <strong>
+            {total} EGP
+            <span className={`order-method ${method}`}>{method}</span>
+          </strong>
+        </div>
+      </div>
+      <div className="order-details">
+        {cartItems.map((item) => (         
+          <div key={item._id} className="item">
+            <div className="border p-2">
+              <img
+                src={item.product.imageCover}
+                alt={item.product.title}
+                className="w-100"
+              />
+              <h6 className="text-center pt-2 small fw-bolder">
+                {item.product.title.split(" ").slice(0, 2).join(" ")}
+              </h6>
+              <p className="small m-0">
+                <span className="text-main">Quantity:</span> {item.count}
+              </p>
+              <p className="small m-0">
+                <span className="text-main">Price:</span> {item.price} EGP
+              </p>
+              <p className="small m-0">
+                <span className="text-main">Total:</span>{" "}
+                {item.price * item.count} EGP
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,9 +1,38 @@
-import "./orders.css";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth } from "../../hooks/useAuth.js";
+import "./Orders.css";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen.jsx";
+import OrderItem from "../../components/OrderItem/OrderItem.jsx";
 
 function Orders() {
+  const { token } = useAuth();
+  const { id: userId } = jwtDecode(token);
+
+  function getAllOrders() {
+    return axios.get(
+      `https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`
+    );
+  }
+  const { data: allOrders, isLoading } = useQuery({
+    queryKey: ["allOrders"],
+    queryFn: getAllOrders,
+    select: (res) => res.data,
+  });
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <section className="orders container">
       <h1 className="section-title">Orders</h1>
+      <div className="orders-list">
+        {allOrders.reverse().map((order) => (
+          <OrderItem key={order.id} order={order} />
+        ))}
+      </div>
     </section>
   );
 }

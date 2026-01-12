2d6ef6b6461b5720c952321c0ffe73c07072bf2c
Belal Sameh <belalsameh.dev@gmail.com>
2026-01-13 00:50:49 +0200

    feat(notfound): build not found page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
 import "./notFound.css";
+import NotFoundImg from "../../assets/error.svg";
 
 function NotFound() {
   return (
-    <section className="not-found container">
-      <h1 className="section-title">NotFound</h1>
+    <section className="not-found">
+      <img src={NotFoundImg} alt="404 image" className="mb-3" />
+      <h1 className="section-title">page not found</h1>
     </section>
   );
 }

21046aa48f04209230b234a681eb5454550d6b5b
Belal Sameh <belalsameh.dev@gmail.com>
2026-01-13 00:19:15 +0200

    feat(products): build products page with reusable product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,93 @@
-import "./productCard.css";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { useWish } from "../../hooks/useWish.js";
+// import { useCart } from "../../hooks/useCart.js";
+import "./ProductCard.css";
 
-function ProductCard() {
+function ProductCard({ product }) {
+  // const { addProductToWish, removeProductFromWish, wishIds } = useWish();
+  // const { addProductToCart } = useCart();
+  const [addingId, setAddingId] = useState(null);
+  
+  // const isFavorite = wishIds.has(product.id);
+  const isAdding = addingId === product.id;
+
+  // function stop(e, fn) {
+  //   e.stopPropagation();
+  //   fn();
+  // }
+
+  // function handleFavorite(productId) {
+  //   if (isFavorite) {
+  //     toast.promise(removeProductFromWish(productId), {
+  //       loading: "Removing...",
+  //       success: "Product removed successfully",
+  //       error: "Failed to remove product",
+  //     });
+  //   } else {
+  //     toast.promise(addProductToWish(productId), {
+  //       loading: "Adding...",
+  //       success: "Product added successfully",
+  //       error: "Failed to add product",
+  //     });
+  //   }
+  // }
+
+  // function handleAddToCart(productId) {
+  //   setAddingId(productId);
+  //   toast.promise(addProductToCart(productId), {
+  //       loading: "Adding...",
+  //       success: "Product added successfully",
+  //       error: "Failed to add product",
+  //   }).finally(() => setAddingId(null));
+  // }
+
   return (
-    <section className="product-card container">
-      <h1 className="section-title">ProductCard</h1>
-    </section>
+    <div className="product-card">
+      <div className="img-container">
+        <img src={product.imageCover} alt="product" className="product-img" />
+      </div>
+      <h6 className="product-category">{product.category.name}</h6>
+      <h6 className="product-title">
+        {product.title.split(" ").slice(0, 2).join(" ")}
+      </h6>
+      <div className="price-rating-container">
+        <div className="price">
+          <span>{product.price}</span>EGP
+        </div>
+        <div className="rating">
+          <FontAwesomeIcon icon="fa-solid fa-star" />
+          {product.ratingsAverage}
+        </div>
+      </div>
+      <button
+        // onClick={(e) => stop(e, () => handleAddToCart(product.id))}
+        disabled={isAdding}
+        type="button"
+        className={`btn cart ${isAdding && "adding"}`}
+      >
+        {isAdding ? (
+          <FontAwesomeIcon
+            icon="fa-solid fa-cart-arrow-down"
+            className="text-main"
+          />
+        ) : (
+          <FontAwesomeIcon icon="fa-solid fa-cart-plus" />
+        )}
+      </button>
+      <button
+        // onClick={(e) => stop(e, () => handleFavorite(product.id))}
+        type="button"
+        className="btn wishlist"
+      >
+        {/* {isFavorite ? (
+          <FontAwesomeIcon icon="fa-solid fa-heart" className="text-danger" />
+        ) : ( */}
+          <FontAwesomeIcon icon="fa-regular fa-heart" />
+        {/* )} */}
+      </button>
+    </div>
   );
 }
 
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,47 @@
-import "./products.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import "./Products.css";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen.jsx";
+import ProductCard from "../../components/ProductCard/ProductCard.jsx";
 
 function Products() {
+  const navigate = useNavigate();
+
+  function getAllProducts() {
+    return axios.get("https://ecommerce.routemisr.com/api/v1/products");
+  }
+  const { data: allProducts, isLoading } = useQuery({
+    queryKey: ["allProducts"],
+    queryFn: getAllProducts,
+    select: (res) => res.data.data,
+  });
+
+  function getProductId(productId) {
+    navigate(`/products/${productId}`);
+  }
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
-    <section className="products container">
-      <h1 className="section-title">Products</h1>
-    </section>
+    <>
+      <section className="products container">
+        <h2 className="section-title">Products</h2>
+        <div className="row g-4">
+          {allProducts?.map((product) => (
+            <div
+              key={product.id}
+              onClick={() => getProductId(product.id)}
+              className="col-lg-3 col-md-4 col-sm-6"
+            >
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
 
